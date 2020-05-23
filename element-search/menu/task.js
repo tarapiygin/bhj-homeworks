f56e4bd323ebb8d9db9877e55eee2d7306e9dc5a
Vassilyev Denis <tarapiygin@gmail.com>
2020-05-23 19:08:45 +0300

    added closing submenu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,19 @@
 
 function initMenuLinks() {
+    const mainMenu = Array.from(document.getElementsByClassName('menu_main'));
     const menuLinks = Array.from(document.getElementsByClassName('menu__link'));
+    mainMenu.forEach(element => {
+        element.addEventListener('click', clickHandler, );
+    });
     menuLinks.forEach(element => {
         element.addEventListener('click', clickHandler);
     });
 }
 
-function clearSubMenu(menuLink) {
-    const menu = menuLink.closest('.menu_main');
-    const subitems = Array.from(menu.getElementsByClassName('menu_sub'));
-    subitems.forEach(element => {
+function clearSubMenu(menuItem) {
+    const menu = menuItem.closest('.menu_main');
+    const subItems = Array.from(menu.getElementsByClassName('menu_sub'));
+    subItems.forEach(element => {
         if (element.classList.contains('menu_active')) {
             element.classList.remove('menu_active');
         };
@@ -17,13 +21,19 @@ function clearSubMenu(menuLink) {
 };
 
 function clickHandler(event) {
-    const subMenu = Array.from(event.currentTarget.closest('.menu__item').getElementsByClassName('menu_sub'));
-    if (subMenu.length !== 0) {
-        event.preventDefault()
-        clearSubMenu(event.currentTarget);
-        subMenu.forEach(element => {
-            element.classList.add('menu_active');
-        });
-    };
+    debugger;
+    if (event.currentTarget.classList.contains('menu_main')) {
+        clearSubMenu(this);
+    } else {
+        event.stopPropagation()
+        const subMenu = Array.from(event.currentTarget.closest('.menu__item').getElementsByClassName('menu_sub'));
+        if (subMenu.length !== 0) {
+            event.preventDefault()
+            clearSubMenu(event.currentTarget);
+            subMenu.forEach(element => {
+                element.classList.add('menu_active');
+            });
+        };
+    }
 };
 initMenuLinks()
