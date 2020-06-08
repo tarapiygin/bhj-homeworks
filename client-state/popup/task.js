56bdd0a6622d1eb987ce8bd53ed8bae137d84880
Vassilyev Denis <tarapiygin@gmail.com>
2020-06-08 20:34:55 +0300

    fixed second task

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -3,26 +3,27 @@
 class PopUp {
     constructor() {
         this.element = document.getElementById('subscribe-modal');
-        this.popUpstate = localStorage.getItem('popUpstate');
+        this.popUpstate = getCoockie('popupstate');
         this.closeButton = this.element.getElementsByClassName('modal__close')[0]
 
         this.registerEvents();
     }
     show() {
-        if(this.popUpstate !== 'true'){
+        if (this.popUpstate !== 'true') {
             this.element.classList.add('modal_active');
-            localStorage.popUpstate = true;
         }
     }
     disable() {
         this.element.classList.remove('modal_active');
+        // let date = new Date(new D);
+        document.cookie = 'popupstate=true';
     }
-    registerEvents(){
+    registerEvents() {
         const popUp = this;
-        function showPopUp(){
+        function showPopUp() {
             popUp.show();
         }
-        function disablePopUp(){
+        function disablePopUp() {
             popUp.disable();
         }
         window.addEventListener('load', showPopUp);
@@ -30,3 +31,12 @@ class PopUp {
     }
 }
 new PopUp();
+
+
+function getCoockie(name) {
+    const value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+    if (parts.length === 2) {
+        return parts.pop().split(";").shift();
+    }
+}
